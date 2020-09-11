cee79e59aa1dc0770b276070ab762f29930ca5f1
VictoriaMarjina <61832815+VictoriaMarjina@users.noreply.github.com>
2020-09-11 17:11:00 +0400

    homework

---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+
+namespace Zadanie1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           
+            int w = 0;
+            int m = 0;
+            Student database = new Student();
+            database = new Student(m);
+            int j = 0;
+            int x = 0;
+            Aspirant database2 = new Aspirant();
+            database2 = new Aspirant(x);
+            Switch(database,database2,w,m,j,x);
+
+        }
+
+        public static void Switch(Student database,Aspirant database2, int w, int m, int j,int x)
+        {
+            Console.WriteLine("нажмите 1 чтобы добавить студента");
+            Console.WriteLine("нажмите 2 чтобы добавить аспиранта");
+            Console.WriteLine("нажмите 3 для вывода информации о студентах");
+            Console.WriteLine("нажмите 4 для вывода информации об аспирантах");
+            Console.WriteLine("нажмите 5 для выхода");
+            string Menyu = Console.ReadLine();
+            switch (Menyu)
+            {
+                case "1":
+                    Console.WriteLine("Сколько студентов вы хотите добавить в базу?");
+                    int a = InNum();
+                    Array.Resize(ref database.data, m + a);
+                    database = Student.AddStu(database,database2, a, w, m+a,j,x);
+                   
+                    w=w+a;
+                    break;
+                case "2":
+                    Console.WriteLine("Сколько аспирантов вы хотите добавить в базу?");
+                    int b = InNum();
+                    Array.Resize(ref database2.dataAsp, m + b);
+                    database2 = Aspirant.AddAsp(database,database2, b, j, x+b, w,m);
+                    j=j+b;
+                    break;
+                case "3":
+                    Student.PrintInfoStu(database,database2,m, w,j,x);
+                    break;
+                case "4":
+                    Aspirant.PrintInfoStu(database,database2, x, w,j,m);
+                    break;
+                case "5":
+                    break;
+                default:
+                    break;
+            }
+           
+        }
+        public static bool CheckNum(string a)
+        {
+            if (int.TryParse(a, out int b))
+                return true;
+            else
+                return false;
+        }
+        public static bool CheckStr(string a)
+        {
+            char[] ch = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < ch.Length; j++)
+                {
+                    if (a[i] == ch[j])
+                        return false;
+                }
+            }
+            return true;
+        }
+        public static int InNum()
+        {
+
+            string a = Console.ReadLine();
+            bool b = CheckNum(a);
+            if (b == true)
+            {
+                int c = Convert.ToInt32(a);
+                return c;
+            }
+            else
+            {
+                Console.Write("не коректные данные повторите ввод:\t");
+                return InNum();
+            }
+
+        }
+        public static string InStr()
+        {
+
+            string a = Console.ReadLine();
+            bool b = CheckStr(a);
+            if (b == true)
+                return a;
+            else
+            {
+                Console.Write("не коректные данные повторите ввод:\t");
+                return InStr();
+            }
+
+        }
+        public static string InZachet()
+        {
+            int a = InNum();
+            if (a < 10000000 && a > 99999999)
+            {
+                string b = Convert.ToString(a);
+                return b;
+            }
+            else
+            {
+                Console.Write("номер зачетной книжки дожен быть не меньше и не длиннее 8символов");
+                return InZachet();
+            }
+        }
+
+    }
+
+    public class Student
+    {
+        private string name;
+        private string lastName;
+        private int kurs;
+        public int Kurs { 
+            set {
+                if (value < 0 || value > 10)
+                {
+                    Console.WriteLine("Номер курса должен быть задан в диапазоне от 1 до 10");
+                    Program.Main();
+                }
+                else
+                {
+                    kurs = value;
+                }
+            }
+            get { return kurs; }
+        }
+        private int zachet;
+        public static int n;
+        public static int N { get; set; }
+        public  Student[] data;
+        public Student(int n)
+        {
+            data = new Student[n];
+        }
+        public Student()
+        {
+           
+        }
+        public Student(string name, string lastName, int kurs, int zachet, int n)
+        {
+            data = new Student[n];
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public int Zachet
+        {
+            get { return zachet; }
+            set { zachet = value; }
+        }
+        public Student this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+        public static Student AddStu(Student database,Aspirant database2, int a, int i, int m,int j,int x)
+        {
+            if (a != 0)
+                {
+                    database[i] = new Student();
+                    Console.WriteLine("введите имя студента");
+                    database[i].Name = Program.InStr();
+                    Console.WriteLine("введите фамилию студента");
+                    database[i].LastName = Program.InStr();
+                    Console.WriteLine("введите курс обучения");
+                    database[i].Kurs = Program.InNum();
+                    Console.WriteLine("введите номер зачетной книжки");
+                    database[i].Zachet = Program.InNum();
+               /* Console.WriteLine(database[0].Name);*/
+                return AddStu(database,database2, a - 1, i + 1, m, j,x);
+                }
+                else
+                {
+                    AddMoreStud(database,database2, i, m, j,x);
+                    return database;
+                } 
+           
+
+        }
+        public static Student AddMoreStud(Student database, Aspirant database2, int i, int m, int j,int x)
+        {
+            Console.WriteLine("чтобы добавить ещё 1 студента нажмите 1");
+            Console.WriteLine("чтобы выйти в меню нажмите любую цифру");
+            int a = Program.InNum();
+            if (a == 1)
+            {
+                Array.Resize(ref database.data, m + a);
+                return AddStu(database, database2, a,  m, m + a,j,x);
+            }
+            else {
+
+                Program.Switch(database,database2, i, m,j,x);
+                return database;
+            }
+    
+
+        }
+        public static void PrintInfoStu(Student database, Aspirant database2, int m, int w,int j,int x)
+        {
+            if (m!=0)
+            {
+                Console.WriteLine("Для информации об одном из студентов нажмите 1 \n Для вывода информации о всех студентах в базе данных нажмите 2 \n Если передумали нажмите 3");
+                string info = Console.ReadLine();
+                switch (info)
+                {
+                    case "1":
+                        Console.WriteLine("Введите номер студента в базе данных");
+                        int a = Program.InNum();
+                        PrintInfo(database,database2,a,w,m,j,x);
+                        break;
+                    case "2":
+                        PrintAllInfo(database,database2,m,w,j,x);
+                        break;
+                    case "3":
+                        Program.Switch(database,database2, w, m,j,x);
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Информация о студентах не была добавлена");
+                Program.Main();
+            }
+
+        }
+
+        public static void PrintInfo(Student database, Aspirant database2, int i,int w,int m,int j, int x)
+        {
+
+            Console.WriteLine($"имя фамилия студента: {database[i].Name} {database[i].LastName} \n номер зачетной книги: {database[i].Zachet}  \n курс обучения: {database[i].Kurs}");
+            Program.Switch(database,database2, w, m,j, x);
+        }
+        public static void PrintAllInfo(Student database, Aspirant database2, int m, int w, int j,int x)
+        {
+
+            for (int i = 0; i < m; i++)
+            {
+
+                Console.WriteLine($"имя фамилия студента: {database[i].Name} {database[i].LastName} \n номер зачетной книги: {database[i].Zachet}  \n курс обучения: {database[i].Kurs}");
+
+            }
+            Program.Switch(database,database2, w, m,j,x);
+        }
+    }
+
+
+    public class Aspirant : Student
+    {
+        private string diss;
+       
+        public Aspirant[] dataAsp;
+        public Aspirant() { }
+        public Aspirant(int n)
+        {
+            dataAsp = new Aspirant[n];
+        }
+        public Aspirant(string name, string lastName, int kurs, int zachet, string diss, int n)
+            : base(name, lastName, kurs, zachet, n)
+        {
+            dataAsp = new Aspirant[n];
+        }
+        public string Diss
+        {
+            get { return diss; }
+            set { diss = value; }
+        }
+        public new Aspirant this[int indexAsp]
+        {
+            get
+            {
+                return dataAsp[indexAsp];
+            }
+            set
+            {
+                dataAsp[indexAsp] = value;
+            }
+        }
+        public static Aspirant AddAsp(Student database,Aspirant database2, int a, int i, int x,int w,int m)
+        {
+            if (a != 0)
+            {
+                database2[i] = new Aspirant();
+                Console.WriteLine("введите имя студента");
+                database2[i].Name = Program.InStr();
+                Console.WriteLine("введите фамилию студента");
+                database2[i].LastName = Program.InStr();
+                Console.WriteLine("введите курс обучения");
+                database2[i].Kurs = Program.InNum();
+                Console.WriteLine("введите номер зачетной книжки");
+                database2[i].Zachet = Program.InNum();
+                Console.WriteLine("введите тему диссертации");
+                database2[i].Diss = Program.InStr();
+              /*  Console.WriteLine(database2[0].Name);*/
+
+                return AddAsp(database, database2, a - 1, i + 1, x,w,m);
+            }
+            else
+            {
+                AddMoreAsp(database, database2, i, x,w,m);
+                return database2;
+            }
+
+
+        }
+        public static Aspirant AddMoreAsp(Student database,Aspirant database2, int i, int x,int w,int m)
+        {
+            Console.WriteLine("чтобы добавить ещё 1 аспиранта нажмите 1");
+            Console.WriteLine("чтобы выйти в меню нажмите любую цифру");
+            int a = Program.InNum();
+            if (a == 1)
+            {
+                Array.Resize(ref database2.dataAsp, x + a);
+                return AddAsp(database,database2, a, x, x + 1,w,m);
+            }
+            else
+            {
+
+                Program.Switch(database, database2, w, m,i,x);
+                return database2;
+            }
+
+
+        }
+        public static new void PrintInfoStu(Student database, Aspirant database2, int x, int w,int j,int m)
+        {
+            if (x != 0)
+            {
+                Console.WriteLine("Для информации об одном из студентов нажмите 1 \n Для вывода информации о всех студентах в базе данных нажмите 2 \n Если передумали нажмите 3");
+                string info = Console.ReadLine();
+                switch (info)
+                {
+                    case "1":
+                        Console.WriteLine("Введите номер студента в базе данных");
+                        int a = Program.InNum();
+                        PrintInfo(database, database2, a, x, w,j,m);
+                        break;
+                    case "2":
+                        PrintAllInfo(database,database2, x, w,j,m);
+                        break;
+                    case "3":
+                        Program.Switch(database,database2, w, m,j,x);
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Информация о студентах не была добавлена");
+                Program.Main();
+            }
+
+        }
+
+        public static new void PrintInfo(Student database, Aspirant database2, int i, int x, int w, int j,int m)
+        {
+
+            Console.WriteLine($"имя фамилия студента: {database2[i].Name} {database2[i].LastName} \n номер зачетной книги: {database2[i].Zachet}  \n курс обучения: {database2[i].Kurs} \n тема диссертации: {database2[i].Diss}");
+            Program.Switch(database,database2, w, m,j,x);
+        }
+        public static new void PrintAllInfo(Student database,Aspirant database2, int x, int w, int j, int m)
+        {
+
+            for (int i = 0; i < x; i++)
+            {
+
+                Console.WriteLine($"имя фамилия студента: {database2[i].Name} {database2[i].LastName} \n номер зачетной книги: {database2[i].Zachet}  \n курс обучения: {database2[i].Kurs} \n тема диссертации: {database2[i].Diss}");
+
+            }
+            Program.Switch(database,database2, w, m,j,x);
+        }
+
+    }
+}
---
+++ Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+
+namespace Zadanie2
+/* Создать базовый класс Счет, котороый содержит информацию - сумма, номер счета, дата открытия, а также методы Выдача суммы и Выдача даты открытия счета.С помошью механизма наследования создать класс "Счет физического лица" и "Счет юридического лица.
+
+"Счет физического лица" содержит:
+поле "Вид счета"
+методы "Начисление процентов" и "Снятие денег со счета"
+
+"Счет юридического лица содержит:
+метод начисления процентов*/
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите номер счета (10000-10009)");
+            string accountnumber = Console.ReadLine();
+            try
+            {
+                int acc = Convert.ToInt32(accountnumber);
+                if (acc >= 10000 && acc <= 10009)
+                {
+                    DataBase(accountnumber);
+                }
+                else
+                {
+                    Console.WriteLine("Указанный счет отсутствует в базе");
+                    Main();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Введите число в диапазоне от 10000 до 10009");
+                Main();
+            }
+        }
+
+        public static void DataBase(string accountnumber)
+        {
+            string[,] accounts = new[,] { { "10000","Александр Сергеевич Пушкин", "физ.лицо", "RUB", "депозитный счет", "300446", "25.08.2015", "" },
+                                       { "10001","Лев Николаевич Толстой", "юр.лицо", "USD", "депозитный счет", "151523,45", "21.01.2019","Tolstoy MMC" },
+                                       { "10002","Антон Павлович Чехов", "физ.лицо", "AZN", "кредитный счет", "-1689", "01.05.2020", "" },
+                                       { "10003","Николай Васильевич Гоголь", "юр.лицо", "AZN", "дебетовый счет", "8789", "05.09.2010", "Qoqol-Moqol ASC" },
+                                       { "10004","Федор Михайлович Достоевский", "физ.лицо", "AZN", "дебетовый счет", "5 689", "25.08.2015", ""  },
+                                       { "10005","Гавриил Романович Державин", "физ.лицо", "RUB", "дебетовый счет", "5 689", "25.08.2015", ""  },
+                                       { "10006","Иван Андреевич Крылов", "юр.лицо", "USD", "дебетовый счет", "5 689", "25.08.2015", "Redbull QSC"  },
+                                       { "10007","Федор Иванович Тютчев", "физ.лицо", "AZN", "кредитный счет", "-5 689", "25.08.2015", "-"  },
+                                       { "10008","Николай Алексеевич Некрасов", "физ.лицо", "AZN", "кредитный счет", "-5 689", "25.08.2015", ""  },
+                                       { "10009","Михаил Евграфович Салтыков-Щедрин", "юр.лицо", "USD", "дебетовый счет", "5 689", "25.08.2015", "Sosial MMC" } };
+
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (accounts[i, 0] == accountnumber)
+                {
+
+                    int sum = Convert.ToInt32(accounts[i, 5]);
+                    string accounttype = accounts[i, 4];
+                    string openingDate = accounts[i, 6];
+                    string company = accounts[i, 7];
+                    string currency = accounts[i, 3];
+                    string clientType = accounts[i, 2];
+
+                    Console.WriteLine($"{accounts[i, 1]}, \n {clientType}, \n {currency}, \n {accounttype}, \n {company}, \n");
+
+                    BankAccount client = new BankAccount(sum, accountnumber, openingDate, clientType, currency, accounttype);
+                    switch (client.ClientType)
+                    {
+                        case "физ.лицо":
+                            Individual client1 = new Individual(sum, accountnumber, openingDate, accounttype, clientType, currency);
+                            if (accounttype == "депозитный счет")
+                            {
+                                client1.PrintDate();
+                                client1.PrintSum();
+                                client1.Deposit();
+                                Main();
+
+                            }
+                            else if (accounttype == "Кредитный счет")
+                            {
+                                client1.PrintDate();
+                                client1.PrintSum();
+                                client1.CashOut();
+                                Main();
+                            }
+                            else
+                            {
+                                
+                                client1.PrintDate();
+                                client1.Credit();
+                                Main();
+
+                            }
+
+                            break;
+
+                        case "юр.лицо":
+
+                            Entity client2 = new Entity(sum, accountnumber, openingDate, accounttype, clientType, currency, company);
+                            client2.PrintDate();
+                            client2.PrintSum();
+                            client2.Deposit();
+                            Main();
+                            break;
+                    }
+                    break;
+
+                }
+
+                else if(accounts[i, 0] != accountnumber)
+                {
+                    continue;
+                   
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+}
+public class BankAccount
+{
+    private static int sum;
+    public static int Sum { get; set; }
+    private string accountnumber;
+    public string Accountnumber { get; set; }
+    private string openingDate;
+    public string OpeningDate { get; set; }
+    private string clientType;
+    public string ClientType { get; set; }
+    private string currency;
+    public string Currency { get; set; }
+    private string accounttype;
+    public string Accounttype { get; set; }
+    public BankAccount(int sum, string accountnumber, string openingDate, string clientType, string currency, string accounttype)
+    {
+        Sum = sum;
+        Accountnumber = accountnumber;
+        OpeningDate = openingDate;
+        ClientType = clientType;
+        Accounttype = accounttype;
+        Currency = currency;
+    }
+
+    public void PrintSum()
+    {
+        Console.WriteLine($"Баланс равен: {Sum} {Currency}");
+    }
+    public void PrintDate()
+    {
+        Console.WriteLine($"Дата открытия данного счета: {OpeningDate}");
+    }
+
+
+}
+public class Individual : BankAccount
+{
+    private string accounttype;
+    public string Accounttype { get; set; }
+    private int interest;
+    public int Interest { get; set; }
+    private int days;
+    public int Days { get; set; }
+    /*private static int newSum;
+    public static int NewSum { get; set; }*/
+
+    public Individual(int sum, string accountnumber, string openingDate, string accounttype, string clientType, string currency) : base(sum, accountnumber, openingDate, clientType, currency, accounttype)
+    {
+        Accounttype = accounttype;
+        Interest = interest;
+        Days = days;
+    }
+
+    public int Deposit()
+    {
+
+        int interest = 11;
+        Console.WriteLine("Введите количество дней пребывания денег на депозитном счету чтобы узнать как изменится баланс счета");
+        try
+        {   
+            int days = Convert.ToInt32(Console.ReadLine());
+            if(days>0){
+                int Depsum = (Sum * interest * days / 365) / 100;
+                Console.WriteLine($"Баланс будет равен{Sum + Depsum} {Currency}");
+                return Depsum;
+            }
+            else
+            {
+                Console.WriteLine("Введите целое положительное число");
+                return Deposit();
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Введите целое положительное число");
+            return Deposit();
+        }
+        
+        
+
+    }
+    public int CashOut()
+    {
+        Console.WriteLine("Какую сумму вы хотите снять со счета?");
+        int moneyout = Convert.ToInt32(Console.ReadLine());
+        Sum = Sum - moneyout;
+        Console.WriteLine($"Баланс будет равен {Sum} {Currency}");
+        return Sum;
+    }
+    public void Credit()
+    {
+        Sum = Math.Abs(Sum);
+        Console.WriteLine($"Задолженность составляет {Sum} {Currency}");
+
+    }
+
+}
+public class Entity : Individual
+{
+    private string company;
+    public string Company { get; set; }
+    public Entity(int sum, string accountnumber, string openingDate, string accounttype, string clientType, string currency, string company) : base(sum, accountnumber, openingDate, accounttype, clientType, currency)
+    {
+        Company = company;
+    }
+    public new int Deposit()
+    {
+        int interest = 8;
+        Console.WriteLine("Введите количество дней пребывания денег на депозитном счету чтобы узнать как изменится баланс счета");
+        try
+        {
+            int days = Convert.ToInt32(Console.ReadLine());
+            if (days > 0)
+            {
+                int Depsum = (Sum * interest * days / 365) / 100;
+                Console.WriteLine($"Баланс будет равен{Sum + Depsum} {Currency}");
+                return Depsum;
+            }
+            else
+            {
+                Console.WriteLine("Введите целое положительное число");
+                return Deposit();
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Введите целое положительное число");
+            return Deposit();
+        }
+
+    }
+}
